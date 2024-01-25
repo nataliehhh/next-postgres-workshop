151015976485511d0354c3cba4300975ac4ee3be
natalie <hardman.natalie85@gmail.com>
2024-01-25 12:45:47 +0000

    add category table to form input

--- src/app/addbeer/page.js
+++ src/app/addbeer/page.js
@@ -3,7 +3,11 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import SavePostButton from "@/components/SavePostButton"
 
-export default function AddBeer() {
+export default async function AddBeer() {
+    const categoryOptions = await sql`
+    SELECT * FROM category_table;
+    `;
+
     async function handleSaveBeer(formData) {
         "use server";
         console.log("Saving post to the database...");
@@ -13,7 +17,9 @@ export default function AddBeer() {
         const brewery = formData.get("brewery");
         const abv = formData.get("abv");
 
-        await sql`INSERT INTO beers (beer_name, style, brewery, abv) VALUES (${beer_name}, ${style}, ${brewery}, ${abv})`;
+        const categoryId = formData.get("category");
+
+        await sql`INSERT INTO beers (beer_name, category_id, style, brewery, abv) VALUES (${beer_name}, ${categoryId}, ${style}, ${brewery}, ${abv})`;
         console.log("Post saved!");
 
         revalidatePath("/beers");
@@ -24,6 +30,14 @@ export default function AddBeer() {
        <form action={handleSaveBeer}>
         <label htmlFor="beer_name">Beer Name</label>
         <input id="beer_name" name="beer_name" type="text" />
+
+        <label htmlFor="category">Category</label>
+        <select id="category" name="category" >
+            {categoryOptions.rows.map((option) => (
+                <option key={option.id} value={option.id}>{option.category}
+                </option>
+            ))}
+        </select>
 
         <label htmlFor="style">Beer Style</label>
         <input id="style" name="style" type="text" />
--- src/queries.js
+++ src/queries.js
@@ -1,17 +1,34 @@
 `CREATE TABLE beers (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    beer_name VARCHAR(255),
+    category_id INTEGER REFERENCES category_table(id),
     style VARCHAR(255),
     brewery VARCHAR(255),
     abv DECIMAL(4, 2)
 );`
 
-   `INSERT INTO beers (name, style, brewery, abv) VALUES
-    ('Carmen', 'Pale Ale', 'Carnival', 5.0),
-    ('Mosaic', 'Pale Ale', 'Neptune', 4.5),
-    ('South Down Baxter', 'West Coast IPA', 'Blacklodge', 6.5),
-    ('Hicky The Rake', 'Pale Ale', 'Wylam', 4.0),
-    ('Saison Provision 2023', 'Mixed Ferment Saison', 'Burning Sky', 7.0),
-    ('Rivalita', 'Imperial Stout', 'Pollys', 9.0),
-    ('Lambic Geuze', 'Lambic', '3 Fonteinen', 6.0),
-    ('Wee Mcash', 'Bitter', 'Five Kingdoms', 4.5);`
+`CREATE TABLE category_table (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(255)
+);`
+
+`INSERT INTO category_table (category) VALUES
+('Pale Ale'),
+('IPA'),
+('Brown Ale'),
+('Dark Ale'),
+('Fruit/Sour'),
+('Farmhouse');`
+
+`INSERT INTO beers (beer_name, category_id, style, brewery, abv) VALUES
+('Carmen', '1', 'Mosaic Pale Ale', 'Carnival', 5.0),
+('Mosaic', '1', 'Mosaic Pale Ale', 'Neptune', 4.5),
+('South Down Baxter', '2', 'West Coast IPA', 'Blacklodge', 6.5),
+('Hicky The Rake', '1', 'Lemonata Pale', 'Wylam', 4.0),
+('Saison Provision 2023', '6', 'Mixed Ferment Saison', 'Burning Sky', 7.0),
+('Rivalita', '4', 'Imperial Stout', 'Pollys', 9.0),
+('Lambic Geuze', '5', 'Lambic', '3 Fonteinen', 6.0),
+('Wee Mcash', '3', 'Bitter', 'Five Kingdoms', 4.5);`
+
+`SELECT * FROM beers 
+JOIN category_table ON beers.category_id = category_table.id`
